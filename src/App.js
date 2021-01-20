df07fc1a19feb46cab39e37ded07fa0086f2e863
Ricardo Heck Alves <ricardoheckalves@icloud.com>
2021-01-20 13:53:50 -0300

    exercise-one completed.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Cars from './Cars';
+import MyContext from './MyContext';
 
-function App() {
-  return (
-    <Cars />
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      cars: {
+        red: false,
+        blue: false,
+        yellow: false,
+      },
+    }
+    this.moveCar = this.moveCar.bind(this);
+  }
+
+  moveCar(car, side) {
+    this.setState((prevState) => ({
+      cars: {
+        ...prevState.cars,
+        [car]: side,
+      }
+    }))
+  }
+
+  render() {
+    const { cars: { red, blue, yellow } } = this.state;
+    const contextValue = {
+      redCar: red,
+      blueCar: blue,
+      yellowCar: yellow,
+      moveCar: this.moveCar,
+    }
+    return (
+      <MyContext.Provider value={contextValue}>
+        <Cars />
+      </MyContext.Provider>
+    );
+  }
 }
 
 export default App;
